a7ceb33f30f5b2dd655f3ec4f7cd65629f90cfbf
Yahia Agwa <yahia.agwa@gmail.com>
2021-06-08 17:31:26 +0200

    remove unused import

--- lib/src/pages/home/widgets/home_grooming.dart
+++ lib/src/pages/home/widgets/home_grooming.dart
@@ -4,7 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:markaa/src/components/markaa_text_button.dart';
 import 'package:markaa/src/components/product_card.dart';
-import 'package:markaa/src/config/config.dart';
 import 'package:markaa/src/data/models/brand_entity.dart';
 import 'package:markaa/src/data/models/category_entity.dart';
 import 'package:markaa/src/data/models/product_list_arguments.dart';
