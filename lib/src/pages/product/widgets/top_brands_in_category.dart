51ae20f751a648d81520f739d45bced026ac3b54
Yahia Agwa <yahia.agwa@gmail.com>
2021-07-06 16:46:34 +0200

    show loadin

--- lib/src/pages/product/widgets/top_brands_in_category.dart
+++ lib/src/pages/product/widgets/top_brands_in_category.dart
@@ -21,76 +21,132 @@ class TopBrandsInCategory extends StatefulWidget {
 
 class _TopBrandsInCategoryState extends State<TopBrandsInCategory> {
   final ProductRepository productRepository = ProductRepository();
-  List<BrandEntity> brands = [];
-  _loadData() async {
-    print("TopBrandsInCategory =======?");
+  Future _loadData() async {
     var res = await productRepository.getTopBrandByProductCategory(widget.productId, Preload.language);
     if (res['code'] == 'SUCCESS') {
       if (res['brands'].length > 0) {
-        res['brands'].forEach((key, obj) {
-          brands.add(BrandEntity.fromJson(obj));
-        });
+        return res['brands'];
       }
     }
-    setState(() {});
+    return [];
   }
 
   @override
   void initState() {
-    _loadData();
+    _loadTopBtands = _loadData();
     super.initState();
   }
 
+  Future _loadTopBtands;
   @override
   Widget build(BuildContext context) {
-    if (brands.isNotEmpty) {
-      return Container(
-        width: 375.w,
-        color: Colors.white,
-        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
-        margin: EdgeInsets.symmetric(vertical: 10.h),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'home_top_brands'.tr(),
-                style: mediumTextStyle.copyWith(color: greyColor, fontSize: 16.sp),
+    return FutureBuilder(
+        future: _loadTopBtands,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+
+          List<BrandEntity> brands = [];
+          snapshot.data.forEach((key, obj) {
+            brands.add(BrandEntity.fromJson(obj));
+          });
+          return Container(
+            width: 375.w,
+            color: Colors.white,
+            padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+            margin: EdgeInsets.symmetric(vertical: 10.h),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'home_top_brands'.tr(),
+                    style: mediumTextStyle.copyWith(color: greyColor, fontSize: 16.sp),
+                  ),
+                  Divider(),
+                  SizedBox(height: 4.h),
+                  Row(
+                    children: brands
+                        .map((e) => InkWell(
+                              onTap: () {
+                                ProductListArguments arguments = ProductListArguments(
+                                  category: CategoryEntity(),
+                                  subCategory: [],
+                                  brand: e,
+                                  selectedSubCategoryIndex: 0,
+                                  isFromBrand: true,
+                                );
+                                Navigator.pushNamed(context, Routes.productList, arguments: arguments);
+                              },
+                              child: CachedNetworkImage(
+                                imageUrl: e.brandThumbnail,
+                                placeholder: (context, url) => Container(),
+                                errorWidget: (context, url, error) => Icon(Icons.error),
+                                imageBuilder: (context, provider) => Container(
+                                  width: 100.h,
+                                  height: 100.h,
+                                  decoration: BoxDecoration(image: DecorationImage(image: provider, fit: BoxFit.fill)),
+                                ),
+                              ),
+                            ))
+                        .toList(),
+                  ),
+                ],
               ),
-              Divider(),
-              SizedBox(height: 4.h),
-              Row(
-                children: brands
-                    .map((e) => InkWell(
-                          onTap: () {
-                            ProductListArguments arguments = ProductListArguments(
-                              category: CategoryEntity(),
-                              subCategory: [],
-                              brand: e,
-                              selectedSubCategoryIndex: 0,
-                              isFromBrand: true,
-                            );
-                            Navigator.pushNamed(context, Routes.productList, arguments: arguments);
-                          },
-                          child: CachedNetworkImage(
-                            imageUrl: e.brandThumbnail,
-                            placeholder: (context, url) => Container(),
-                            errorWidget: (context, url, error) => Icon(Icons.error),
-                            imageBuilder: (context, provider) => Container(
-                              width: 100.h,
-                              height: 100.h,
-                              decoration: BoxDecoration(image: DecorationImage(image: provider, fit: BoxFit.fill)),
-                            ),
-                          ),
-                        ))
-                    .toList(),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-    return Container();
+            ),
+          );
+        });
+    // if (brands.isNotEmpty) {
+    //   return Container(
+    //     width: 375.w,
+    //     color: Colors.white,
+    //     padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+    //     margin: EdgeInsets.symmetric(vertical: 10.h),
+    //     child: SingleChildScrollView(
+    //       scrollDirection: Axis.horizontal,
+    //       child: Column(
+    //         crossAxisAlignment: CrossAxisAlignment.start,
+    //         children: [
+    //           Text(
+    //             'home_top_brands'.tr(),
+    //             style: mediumTextStyle.copyWith(color: greyColor, fontSize: 16.sp),
+    //           ),
+    //           Divider(),
+    //           SizedBox(height: 4.h),
+    //           Row(
+    //             children: brands
+    //                 .map((e) => InkWell(
+    //                       onTap: () {
+    //                         ProductListArguments arguments = ProductListArguments(
+    //                           category: CategoryEntity(),
+    //                           subCategory: [],
+    //                           brand: e,
+    //                           selectedSubCategoryIndex: 0,
+    //                           isFromBrand: true,
+    //                         );
+    //                         Navigator.pushNamed(context, Routes.productList, arguments: arguments);
+    //                       },
+    //                       child: CachedNetworkImage(
+    //                         imageUrl: e.brandThumbnail,
+    //                         placeholder: (context, url) => Container(),
+    //                         errorWidget: (context, url, error) => Icon(Icons.error),
+    //                         imageBuilder: (context, provider) => Container(
+    //                           width: 100.h,
+    //                           height: 100.h,
+    //                           decoration: BoxDecoration(image: DecorationImage(image: provider, fit: BoxFit.fill)),
+    //                         ),
+    //                       ),
+    //                     ))
+    //                 .toList(),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   );
+    // }
+    // return Container();
   }
 }
