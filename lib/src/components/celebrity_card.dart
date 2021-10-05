39f4e53f412becedb45bc0dc86b0c25adb1dba8a
Yahia Agwa <yahia.agwa@gmail.com>
2021-10-05 10:46:59 +0200

    remove unused import

--- lib/src/components/celebrity_card.dart
+++ lib/src/components/celebrity_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:markaa/src/routes/routes.dart';
 
