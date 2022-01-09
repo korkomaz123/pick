e1da73e86d959b847f70bf277bf37bbcf1ac3ef8
iscojin <flutterdev9715@gmail.com>
2022-01-09 20:57:23 +0300

    switched to dev mode

--- lib/env.dart
+++ lib/env.dart
@@ -1,4 +1,4 @@
 // if true, app is running on development environment,
 // if false, app is running on production environment
-const bool dev = false;
+const bool dev = true;
 const String env = dev ? 'DEV' : 'LIVE';
